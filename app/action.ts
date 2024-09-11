e7c4729301cf3ebe686e48f23b8cc6f7cbaa6cb6
momozadani <mohammad.zadani1375@gmail.com>
2024-09-11 17:07:51 +0200

    updated the database

--- app/action.ts
+++ app/action.ts
@@ -8,12 +8,15 @@ import { prisma } from "./../prisma/prisma";
 import { join } from "path";
 import { writeFile } from "fs/promises";
 import { permanentRedirect, redirect, RedirectType } from "next/navigation";
+import { ADMIN } from "./lib/data";
+import { user } from "@nextui-org/theme";
 
 const schema = z.object({
   size: z.string(),
   color: z.string(),
   stickColor: z.string(),
   location: z.string(),
+  customerNumber: z.string().min(5),
   consent: z.coerce.boolean(),
 });
 const ACCEPTED_FILE_TYPES = ["image/png", "image/jpg"];
@@ -24,11 +27,10 @@ const hoodieSchema = z.object({
   }, "file must a picture"),
   size: z.string(),
   color: z.string(),
-  quantity: z.coerce.number(),
   available: z.coerce.boolean(),
 });
 
-export async function createHoodieAction(
+export async function orderHoodieAction(
   prevState: any,
   formData: FormData | null
 ) {
@@ -36,21 +38,74 @@ export async function createHoodieAction(
   if (!session) {
     redirect("/");
   }
+
+  console.log("this is the result", formData);
   const form = Object.fromEntries(formData!.entries());
   const res = schema.safeParse(form);
-
+  console.log("this is the result", res.error?.errors);
   if (!res.success) {
-    return { message: "error occured" };
+    return { message: res.error.errors };
   }
-
-  return { message: "working" };
+  const { size, color, stickColor, location, consent, customerNumber } =
+    res.data;
+  if (consent === false) {
+    return { message: "not consented" };
+  }
+  const locationRecord = await prisma.location.findFirst({
+    where: {
+      name: location,
+    },
+  });
+  const colorRecord = await prisma.color.findFirst({
+    where: {
+      name: color,
+    },
+  });
+  const sizeRecord = await prisma.size.findFirst({
+    where: {
+      name: size,
+    },
+  });
+  const stickColorRecord = await prisma.location.findFirst({
+    where: {
+      name: stickColor,
+    },
+  });
+  const hoodieVariantRecord = await prisma.hoodieVariant.findFirst({
+    where: {
+      colorId: colorRecord?.id,
+      sizeId: sizeRecord?.id,
+    },
+  });
+  const userSession = session.user!;
+  const name = userSession.name?.split(" ")!;
+  const userRecord = await prisma.user.upsert({
+    where: {
+      email: userSession.email!,
+    },
+    update: {},
+    create: {
+      firstName: name[0],
+      lastName: name[1],
+      email: userSession.email!,
+      customerNumber: customerNumber,
+    },
+  });
+  return { message: "success" };
 }
 
 export async function signInAction() {
   await signIn();
 }
 
-export async function uploadAction(prevState: any, formData: FormData | null) {
+export async function uploadHoodieVariantAction(
+  prevState: any,
+  formData: FormData | null
+) {
+  const session = await auth();
+  if (!session || session?.user.role !== ADMIN) {
+    redirect("/");
+  }
   if (formData === null) {
     return { message: "form should not be empty" };
   }
@@ -58,31 +113,50 @@ export async function uploadAction(prevState: any, formData: FormData | null) {
     file: formData?.get("file"),
     color: formData?.get("color"),
     size: formData?.get("size"),
-    quantity: formData?.get("quantity"),
     available: formData?.get("available"),
   });
 
-  if (validation.success) {
-    try {
-      const buffer = Buffer.from(await validation.data.file.arrayBuffer());
-      const uniqueSuffix = `${new Date(Date.now())}`;
-      console.log(uniqueSuffix);
-      const fileName = uniqueSuffix + validation.data.file.name;
-      const uploadDir = join(process.cwd(), "public");
-      await writeFile(`${uploadDir}/${fileName}`, buffer);
-      // await prisma.hoodieVariant.create({
-      //   data: {
-      //     size: await prisma.size.findFirst(validation.data.size),
-      //      color: validation.data.color,
-      //     quantity: validation.data.quantity,
-      //     available: validation.data.available,
-      //     imagePath: fileName,
-      //   },
-      // });
-      return { message: "ok" };
-    } catch (e) {
-      console.log("an error occured", e);
-      return { message: "error" };
+  if (!validation.success) {
+    return { message: "error in operation" };
+  }
+  const { file, color, size, available } = validation.data;
+  try {
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const uniqueSuffix = `${new Date(Date.now())}`;
+    const fileName = uniqueSuffix + validation.data.file.name;
+    const uploadDir = join(process.cwd(), "public");
+    await writeFile(`${uploadDir}/${fileName}`, buffer);
+    const sizeRecord = await prisma.size.upsert({
+      where: {
+        name: size,
+      },
+      update: {},
+      create: { name: size },
+    });
+    const colorRecord = await prisma.color.upsert({
+      where: {
+        name: color,
+      },
+      update: {},
+      create: { name: color },
+    });
+    if (sizeRecord === null) {
+      return { message: "error size does not exit" };
     }
+    if (colorRecord === null) {
+      return { message: " error color does not exit" };
+    }
+    await prisma.hoodieVariant.create({
+      data: {
+        sizeId: sizeRecord.id,
+        colorId: colorRecord.id,
+        available: available,
+        imagePath: fileName,
+      },
+    });
+    return { message: "successfull" };
+  } catch (e) {
+    console.log("an error occured", e);
+    return { message: "error" };
   }
 }
--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import { Button } from "@nextui-org/button";
-import { createHoodieAction } from "../action";
+import { orderHoodieAction } from "../action";
 import { colors, hoodieSize } from "../lib/data";
 import { Select, SelectItem } from "@nextui-org/select";
 import { RadioGroup, Radio } from "@nextui-org/radio";
 import { useFormState } from "react-dom";
-import { Checkbox } from "@nextui-org/checkbox";
 import { useState } from "react";
+import { Input } from "@nextui-org/input";
+import { Checkbox, CheckboxGroup } from "@nextui-org/checkbox";
 
 export default function Order() {
   const [isSelected, setIsSelected] = useState(false);
-  const [state, formAction, pending] = useFormState(createHoodieAction, {
+  const [state, formAction, pending] = useFormState(orderHoodieAction, {
     message: "input not valid",
   });
+  const [isConsent, setIsConsent] = useState<string[]>([]);
   return (
     <div className="mx-80 min-h-screen flex justify-center">
       <form className="flex flex-col justify-evenly w-1/2" action={formAction}>
@@ -42,16 +44,24 @@ export default function Order() {
           </Select>
         </div>
         <RadioGroup
+          classNames={{
+            label: "text-black",
+          }}
           label="Ich möchte den Stick auf meinem Hoodie in der Farbe..."
           name="stickColor"
+          errorMessage={"Bitte eine Farbe auswählen"}
         >
           <Radio value="schwarz">schwarz</Radio>
           <Radio value="weiß">weiß</Radio>
         </RadioGroup>
 
         <RadioGroup
+          classNames={{
+            label: "text-black",
+          }}
           label="Ich möchte den Stick auf meinem Hoodie in der Farbe..."
           name="location"
+          defaultValue={"tower"}
         >
           <Radio required value="tower">
             in den Tower
@@ -63,18 +73,37 @@ export default function Order() {
         </RadioGroup>
 
         <div>
-          <div>
+          <label htmlFor="consent">
             Ich erkläre mich damit einverstanden, dass die einmalige monatliche
             Eigenleistung in Höhe von 15,00 Euro + 6,99€ für den Versand, falls
             zutreffend von meinem Nettoverdienst einbehalten wird
-          </div>
-          <input
+          </label>
+          <Checkbox
             type="checkbox"
             name="consent"
             className="mt-8"
-            defaultValue={"off"}
+            id="consent"
+            value="true"
+            required
+          >
+            Ja*
+          </Checkbox>
+          <input type="hidden" name="consent" value="false" />
+        </div>
+        <div>
+          <label htmlFor="customerNumber">
+            *Bitte trage hier noch deine Personalnummer ein (diese benötigt HR
+            für die Abrechnung, du findest sie auf deiner digitalen
+            Gehaltsabrechnung)
+          </label>
+          <Input
+            className="pt-3"
+            id="customerNumber"
+            name="customerNumber"
+            type="number"
+            inputMode="numeric"
+            isClearable
           />
-          <span className="ml-4">ja*</span>
         </div>
 
         <Button
